cb876c258d28ed2f8e91fc060a6c83bd3fcad9bf
Gaston Andres Vidal <vidalgaston92@gmail.com>
2022-11-10 10:38:20 +0100

    base project structure

---
+++ springboot-service-items/src/main/java/com/microservice/springboot/app/items/SpringbootServiceItemsApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.springboot.app.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceItemsApplication.class, args);
+	}
+
+}
---
+++ springboot-service-items/src/test/java/com/microservice/springboot/app/items/SpringbootServiceItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.springboot.app.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
